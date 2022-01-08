0a0d6a05682e1fb3988554e3ff86144510bf644d
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-08 05:25:10 -0500

    Remove unused logger

--- src/cldr/__main__.py
+++ src/cldr/__main__.py
@@ -3,14 +3,10 @@
 from __future__ import annotations
 
 import clack
-from logrus import Logger
 
 from ._config import clack_parser
 from ._constants import PROJECT_NAME
 from ._runners import run_build, run_info, run_new
-
-
-logger = Logger(__name__)
 
 
 main = clack.main_factory(
